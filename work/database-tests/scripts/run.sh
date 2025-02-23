034934f69dbe4cf97a98f68a65592aa0cf913a1a
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-02-23 17:16:56 +0000

    get test dlls using folder names

--- work/database-tests/scripts/run.sh
+++ work/database-tests/scripts/run.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-connection_string=$1
-artifacts_dir=$2
+artifacts_dir=$1
 
-sleep 5
-echo "${connection_string}"
+# Get assembly names, for now it's just the names of all the files/dircs in the artifact folder
+artifact_names=($(ls "${artifacts_dir}"))
+
+# For each assembly name, using find, get the hardcoded path to the .dll 
+artifact_paths=()
+
+for artifact_name in "${artifact_names[@]}"
+do
+    path=$(find "${artifacts_dir}" -type f -name "${artifact_name}.dll")
+
+    if [[ ! -z ${path} ]]; then
+        artifact_paths+=("${path}")
+    fi
+done
+
+
+# For each dll, run dotnet test against it.
+for artifact_path in "${artifact_paths[@]}"
+do
+    echo "${artifact_path}"
+done
+
