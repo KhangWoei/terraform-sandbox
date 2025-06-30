70461d75a77a84c3d261bb9176799686e3570359
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-06-30 16:51:04 +0100

    add variable for version

--- src/databases/azuresql/variables.tf
+++ src/databases/azuresql/variables.tf
@@ -6,6 +6,16 @@ variable "resource_group" {
   nullable = false
 }
 
+variable "db_version" {
+  type    = string
+  default = "12.0"
+
+  validation {
+    condition = contains(["2.0", "12.0"], var.db_version)
+    error_message = "Version must be either '2.0' or '12.0'."
+  }
+}
+
 variable "admin_username" {
   type    = string
   default = "azureadmin"
