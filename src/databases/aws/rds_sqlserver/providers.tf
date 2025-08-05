bc58017b2889c426b1dd3f1227b3a6277caf2073
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-08-05 16:09:06 +0100

    use named profile to authenticate

--- src/databases/aws/rds_sqlserver/providers.tf
+++ src/databases/aws/rds_sqlserver/providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.7.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -12,6 +12,5 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
-  // relying on AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY to be set
+  profile = "terraform"
 }
--- src/databases/aws/rds_sqlserver/variables.tf
+++ src/databases/aws/rds_sqlserver/variables.tf
@@ -1,8 +1,3 @@
-variable "aws_region" {
-  type     = string
-  nullable = false
-}
-
 variable "admin_details" {
   type = object({
     username = string
