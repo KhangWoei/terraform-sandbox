3bb71a485968d60b1e137830714620416fc8a514
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-08-05 15:47:55 +0100

    comment

--- src/databases/aws/rds_sqlserver/providers.tf
+++ src/databases/aws/rds_sqlserver/providers.tf
@@ -5,12 +5,13 @@ terraform {
       version = "~> 3.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "~> 3.0"
     }
   }
 }
 
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
+  // relying on AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY to be set
 }
